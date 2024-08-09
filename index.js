82deb06d6f961b54a30fd24563325a7aac1d2179
David Brossard <6230719+davidjbrossard@users.noreply.github.com>
2024-08-08 20:19:36 -0700

    Updated the code to read yaml

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const yaml = require('js-yaml');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const domainFile = core.getInput('domain-file');
+  console.log(`Processing ${domainFile}!`);
+  const yamlData = yaml.load(fs.readFileSync(domainFile, 'utf8'));
+  console.log(yamlData.identity);
+  console.log(yamlData.attributes);
+  console.log(yamlData.policy.mainPolicyId);
 } catch (error) {
   core.setFailed(error.message);
 }
