1d29f726ff756fc4a67199bf06c0d37b5ee62399
David Brossard <6230719+davidjbrossard@users.noreply.github.com>
2024-08-08 23:05:22 -0700

    XML parsing progress

--- index.js
+++ index.js
@@ -2,31 +2,44 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const yaml = require('js-yaml');
 const fs = require('fs');
-const { XMLParser, XMLBuilder, XMLValidator } = require("fast-xml-parser");
+var xpath = require('xpath');
+var dom = require('@xmldom/xmldom').DOMParser;
 
 function parseXACML(xacmlContent) {
-
-    const parser = new XMLParser();
-    let jObj = parser.parse(xacmlContent);
-
-
-    const builder = new XMLBuilder();
-    const xmlContent = builder.build(jObj);
+    var doc = new dom().parseFromString(xacmlContent, 'text/xml');
+    var xacmlns = xpath.useNamespaces({"xacml3": "urn:oasis:names:tc:xacml:3.0:core:schema:wd-17"});
+    // var nodes = xpath.select("//xacml3:PolicySet", doc);
+    var nodes = xacmlns("//xacml3:PolicySet/@PolicySetId",doc);
+    console.log(nodes);
+    for (var node in nodes){
+        for (var attribute in nodes.attributes){
+            // console.log(attribute.name);
+        }
+        console.log('node:'+node);
+    }
+    // console.log(nodes.length);
+    // console.log(nodes[0].localName + ": " + nodes[0].firstChild.data);
+    // console.log("Node: " + nodes[0].toString());
 }
 
 function generateDocumentation(title, attributes, policies) {
     let documentation = "";
     // 1. Add Title
     documentation += "# " + title + "\n";
+    documentation += "## Overview\n";
+    documentation += " - Number of policy entries: "+policies.length+"\n";
+    documentation += " - Number of attributes: "+Object.entries(attributes).length+"\n";
     documentation += "## Policy Overview\n";
-    for (var policy in policies) {
-        p = parseXACML(policy);
+    policies.forEach((policy)=>{
+        let p = parseXACML(policy);
+        console.log();
         documentation += " - " + "some name" + "\n";
-
-    }
+    });
     documentation += "## Attribute Overview\n";
-    for (var attribute in attributes) {
-        documentation += " - " + attribute.xacmlId + "\n";
+    for (const [key, value] of Object.entries(attributes)) {
+        documentation += " - " + value.xacmlId + "\n";
+        documentation += "   - " + value.category + "\n";
+        documentation += "   - " + value.datatype + "\n";
     }
     return documentation;
 }
@@ -44,7 +57,8 @@ try {
         // In case of a error throw err.
         if (err) throw err;
     })
-    console.log('Wrote md file to '+outputFile);
+    console.log('Wrote md file to ' + outputFile);
 } catch (error) {
+    console.error(error);
     core.setFailed(error.message);
 }
