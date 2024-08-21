5da70f5f1c1f99d49556c6ddd81a696ec5da923d
David Brossard <6230719+davidjbrossard@users.noreply.github.com>
2024-08-20 23:08:33 -0700

    Updated the action to generate additional documentation

--- index.js
+++ index.js
@@ -2,38 +2,88 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const yaml = require('js-yaml');
 const fs = require('fs');
-var xpath = require('xpath');
-var dom = require('@xmldom/xmldom').DOMParser;
+const xpath = require('xpath');
+const { isArray } = require('util');
+const dom = require('@xmldom/xmldom').DOMParser;
+// const xmldom = require('@xmldom/xmldom');
+const selector = xpath.useNamespaces({"xacml3": "urn:oasis:names:tc:xacml:3.0:core:schema:wd-17"});
+var warnings=[];
+
+function parseBlocks(blockType, nodes){
+    const parent = {
+        'Rule': 'Policy',
+        'Policy': 'PolicySet',
+        'PolicySet': 'PolicySet'
+    }
+    let blocks = [];
+    for (var i = 0; i < nodes.length; i++) {
+        let node = nodes[i];
+        // console.log("description: "+node.getElementsByTagNameNS("urn:oasis:names:tc:xacml:3.0:core:schema:wd-17", "Description")[0].data);
+        let p = {
+            type: blockType,
+            identifier : node.getAttribute(blockType+'Id').replace('http://axiomatics.com/alfa/identifier/',''),
+            description: selector("xacml3:Description/text()",node),
+            parent: parent[blockType]//node.parentNode.getAttribute(parent[block]+'Id').replace('http://axiomatics.com/alfa/identifier/','')
+        };
+        let ks = Object.keys(node.parentNode);
+        ks.forEach((k)=>{
+            if (k==='attributes'){
+                p.parent = node.parentNode['attributes']['0'].value.replace('http://axiomatics.com/alfa/identifier/','');
+            }
+        });
+        if (p.description.length==0){
+            warnings.push({
+                description: 'Missing documentation',
+                type: blockType,
+                location: p.identifier
+            });
+        }
+        // console.log(p);
+        blocks.push(p);
+    }
+    return blocks;
+}
 
 function parseXACML(xacmlContent) {
-    var doc = new dom().parseFromString(xacmlContent, 'text/xml');
-    var xacmlns = xpath.useNamespaces({"xacml3": "urn:oasis:names:tc:xacml:3.0:core:schema:wd-17"});
-    // var nodes = xpath.select("//xacml3:PolicySet", doc);
-    var nodes = xacmlns("//xacml3:PolicySet/@PolicySetId",doc);
-    console.log(nodes);
-    for (var node in nodes){
-        for (var attribute in nodes.attributes){
-            // console.log(attribute.name);
+    let ps = [];
+    try {
+        let doc = new dom().parseFromString(xacmlContent, 'text/xml');
+        const BLOCK_TYPES = ['PolicySet', 'Policy', 'Rule'];
+        for (var block of BLOCK_TYPES){
+            let nodes = selector('//xacml3:'+block,doc);
+            let bs = parseBlocks(block, nodes);
+            bs.forEach((b)=>{
+                ps.push(b);
+            });
         }
-        console.log('node:'+node);
+        
+    } catch (error) {
+        console.error(error);
     }
-    // console.log(nodes.length);
-    // console.log(nodes[0].localName + ": " + nodes[0].firstChild.data);
-    // console.log("Node: " + nodes[0].toString());
+    return ps;
 }
 
 function generateDocumentation(title, attributes, policies) {
     let documentation = "";
+    let intro = "";
     // 1. Add Title
     documentation += "# " + title + "\n";
     documentation += "## Overview\n";
-    documentation += " - Number of policy entries: "+policies.length+"\n";
+    documentation += " - Number of YAML policy entries: "+policies.length+"\n";
+    documentation += " - Number of XACML Policy Sets: "+policies.length+"\n";
+    documentation += " - Number of XACML Policies: "+policies.length+"\n";
+    documentation += " - Number of XACML Rules: "+policies.length+"\n";
     documentation += " - Number of attributes: "+Object.entries(attributes).length+"\n";
     documentation += "## Policy Overview\n";
+    let i = 1;
     policies.forEach((policy)=>{
-        let p = parseXACML(policy);
-        console.log();
-        documentation += " - " + "some name" + "\n";
+        let ps = parseXACML(policy);
+        ps.forEach((xacmlPolicy)=>{
+            documentation += " - [" + xacmlPolicy.identifier + "](#"+xacmlPolicy.identifier+")\n";
+            if (xacmlPolicy.description.length>0)
+                documentation += "   - "+xacmlPolicy.description+ "\n";
+                documentation += "   - parent: ["+xacmlPolicy.parent+ "](#"+xacmlPolicy.parent+")\n";
+        });
     });
     documentation += "## Attribute Overview\n";
     for (const [key, value] of Object.entries(attributes)) {
@@ -41,6 +91,15 @@ function generateDocumentation(title, attributes, policies) {
         documentation += "   - " + value.category + "\n";
         documentation += "   - " + value.datatype + "\n";
     }
+    if (warnings.length>0){
+        documentation += "## Warnings & Recommendations\n";
+        warnings.forEach((warning)=>{
+            documentation+= " - "+warning.description+"\n";
+            documentation+= "   - "+warning.type+"\n";
+            documentation+= "   - "+warning.location+"\n";
+        });
+    }
+
     return documentation;
 }
 
@@ -49,8 +108,6 @@ try {
     const outputFile = core.getInput('output-file');
     console.log(`Processing ${domainFile}!`);
     const yamlData = yaml.load(fs.readFileSync(domainFile, 'utf8'));
-    console.log();
-    console.log(yamlData.policy.mainPolicyId);
 
     // Write data in 'Output.txt' .
     fs.writeFile(outputFile, generateDocumentation("Documentation for domain " + yamlData.identity, yamlData.attributes, yamlData.policy.xacmlSpecifications), (err) => {
